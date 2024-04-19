8fa7f98bd45ed697fed2909d9e850bd97927aa05
Mitsuoka Takahiro <tmitsuoka0423@gmail.com>
2024-04-19 13:48:44 +0000

    feat: D1 & OpenAI

--- Makefile
+++ Makefile
@@ -3,3 +3,9 @@ local:
 
 deploy:
 	npm run deploy
+
+migrate:
+	wrangler d1 execute line-gpts-db --local --file=./migration/conversations.sql
+
+migrate-production:
+	wrangler d1 execute line-gpts-db --remote --file=./migration/conversations.sql
--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,61 @@
-import { MessageAPIResponseBase, TextMessage, WebhookEvent } from '@line/bot-sdk';
+import { TextEventMessage, WebhookEvent } from '@line/bot-sdk';
 import { Hono } from 'hono';
+import { Line } from './line';
+import { OpenAI } from './openai';
+import { Conversation } from './types/tables';
 
 const app = new Hono();
-app.get('*', (c) => {
-	return c.text('Hello World!');
-});
+
+app.get('*', (c) => c.text('Hello World!'));
 
 app.post('/api/webhook', async (c) => {
+	const lineChannelAccessToken = c.env?.LINE_CHANNEL_ACCESS_TOKEN as string;
+	const openAiApiKey = c.env?.OPENAI_API_KEY as string;
+
 	const data = await c.req.json();
 	const events: WebhookEvent[] = (data as any).events;
-	const accessToken: string = c.env?.LINE_CHANNEL_ACCESS_TOKEN as string;
 
-	if (!accessToken) {
-		throw new Error('LINE チャネルアクセストークンを設定してください');
-	}
-
-	await Promise.all(
-		events.map(async (event: WebhookEvent) => {
-			try {
-				await textEventHandler(event, accessToken);
-			} catch (err: unknown) {
-				if (err instanceof Error) {
-					console.error(err);
-				}
-				return c.json({
-					status: 'error',
-				});
+	const event = events
+		.map((event: WebhookEvent) => {
+			if (event.type != 'message' || event.message.type != 'text') {
+				return;
 			}
+			return event;
 		})
-	);
-	return c.json({ message: 'ok' });
-});
+		.filter((event) => event)[0];
 
-const textEventHandler = async (event: WebhookEvent, accessToken: string): Promise<MessageAPIResponseBase | undefined> => {
-	if (event.type !== 'message' || event.message.type !== 'text') {
-		return;
+	if (!event) {
+		console.log(`No event: ${events}`);
+		return c.json({ message: 'ok' });
 	}
 
 	const { replyToken } = event;
-	const { text } = event.message;
-	const response: TextMessage = {
-		type: 'text',
-		text,
-	};
-	await fetch('https://api.line.me/v2/bot/message/reply', {
-		body: JSON.stringify({
-			replyToken: replyToken,
-			messages: [response],
-		}),
-		method: 'POST',
-		headers: {
-			Authorization: `Bearer ${accessToken}`,
-			'Content-Type': 'application/json',
-		},
-	});
-};
+	const { text: my_message } = event.message as TextEventMessage;
+
+	try {
+		// Fetch 2 conversation from D1
+		const { results }: { results: Conversation[] } = await c.env?.DB?.prepare(`select * from conversations order by id desc limit 2`).all();
+		console.log(results);
+
+		// Generate answer with OpenAI
+		const openaiClient = new OpenAI(openAiApiKey);
+		const generatedMessage = await openaiClient.generateMessage(results, my_message);
+		console.log(generatedMessage);
+		if (!generatedMessage || generatedMessage === '') throw new Error('No message generated');
+
+		// Save generated answer to D1
+		await c.env?.DB?.prepare(`insert into conversations (my_message, bot_message) values (?, ?)`).bind(my_message, generatedMessage).run();
+
+		// Reply to the user
+		const lineClient = new Line(lineChannelAccessToken);
+		await lineClient.replyMessage(generatedMessage, replyToken);
+		return c.json({ message: 'ok' });
+	} catch (err: unknown) {
+		if (err instanceof Error) console.error(err);
+		const lineClient = new Line(lineChannelAccessToken);
+		await lineClient.replyMessage('I am not feeling well right now.', replyToken);
+		return c.json({ message: 'ng' });
+	}
+});
 
 export default app;
