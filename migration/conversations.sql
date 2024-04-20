841babaa5283242aff62f5f0bc5570dfa47d74d5
Mitsuoka Takahiro <tmitsuoka0423@gmail.com>
2024-04-20 06:28:47 +0000

    feat: update tables

--- migration/conversations.sql
+++ migration/conversations.sql
@@ -5,12 +5,3 @@ CREATE TABLE conversations (
   my_message TEXT NOT NULL,
   bot_message TEXT NOT NULL
 );
-
-INSERT INTO
-  conversations (my_message, bot_message)
-VALUES
-  ('How are you?', 'Hi!'),
-  (
-    'What are you doing now?',
-    'I''m reading a book.'
-  );
---
+++ migration/users.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  line_id TEXT NOT NULL
+);
